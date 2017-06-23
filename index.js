5d094bd169c87b6b8f4e92981a90e2633e5e4456
jxcao <jxcao@corp.netease.com>
2017-06-23 15:47:21 +0800

    首次提交

---
+++ index.js
@@ -0,0 +1,26 @@
+/**
+ *  ftl扩展插件
+ *  主要功能 include和import路径补全
+ *  用指定正则方法获取url，并加入监控补全url
+ *  ftl源路径导出
+ *  img link script等文件路径补全类似于  html-loader插件
+ * 
+ *  使用方法请看examples
+ * 
+ * 
+ */
+const plugin = require('./lib/plugin');
+const extract = require('./lib/extract');
+
+function ExtractFtlPlugin () {
+
+}
+// plugin入口
+ExtractFtlPlugin.apply = function () {
+
+}
+
+// loader入口
+ExtractFtlPlugin.extract = function (options) {
+
+}
